1ef134d5a6625df29b31960d41e495da2f194c39
Breno <breno.jgomes@hotmail.com>
2024-08-26 19:24:06 -0300

    Add project files.

---
+++ Postech.GroupEight.ContactIntegration.Worker/Program.cs
@@ -0,0 +1,7 @@
+using Postech.GroupEight.ContactIntegration.Worker;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHostedService<Worker>();
+
+var host = builder.Build();
+host.Run();
