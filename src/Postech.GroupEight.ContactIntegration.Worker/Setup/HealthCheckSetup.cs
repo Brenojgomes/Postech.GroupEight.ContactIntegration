d8b01f3aec3bb5029b932d924ecdefa7533014c9
Brenojgomes <breno.jgomes2@gmail.com>
2024-11-20 14:44:46 -0300

    Add healtcheck

---
+++ src/Postech.GroupEight.ContactIntegration.Worker/Setup/HealthCheckSetup.cs
@@ -0,0 +1,20 @@
+﻿using Postech.GroupEight.ContactIntegration.Infra;
+using Postech.GroupEight.ContactIntegration.Worker.HealthChecks;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Postech.GroupEight.ContactIntegration.Worker.Setup
+{
+    [ExcludeFromCodeCoverage]
+    internal static class HealthCheckSetup
+    {
+        internal static void AddMongoDbHealthCheck(this IHealthChecksBuilder healthChecks)
+        {
+            healthChecks.AddCheck<MongoDbHealthCheck>(nameof(MongoDbOptions));
+        }
+
+        internal static void AddRabbitMQHealthCheck(this IHealthChecksBuilder healthChecks)
+        {
+            healthChecks.AddCheck<MassTransitRabbitMqHealthCheck>("RabbitMQ");
+        }
+    }
+}
